13d511577b7d60d62aa0472b6a56be9f416ba730
a33707 <30279362+a33707@users.noreply.github.com>
2019-05-26 22:25:51 +0900

    Add files via upload

---
+++ 12-2-c.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+#include  <fcntl.h>
+#include  <unistd.h>
+#include  <stdlib.h>
+#include  <string.h>
+
+void  main( )
+{
+	int   fd,   m;
+	char   buf[ 30 ] = "Linux  C  Programming";
+	if(  (fd = open( "test", O_WRONLY | O_CREAT | O_EXCL,  0644 ) ) == -1 ) { 
+		printf("open failed");
+		exit( 1 );
+	}
+	write( fd, buf, strlen(buf) );
+	close( fd );
+}
+
