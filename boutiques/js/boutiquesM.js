29140dae72c04f929590e01c9ca7473ec33df25d
윤창윤 <eddieyoon@ogqcorp.com>
2020-12-28 12:37:56 +0900

    수정

--- boutiques/js/boutiquesM.js
+++ boutiques/js/boutiquesM.js
@@ -127,24 +127,31 @@ $(document).ready(function(){
     $("#layerPopup").hide();
   });
 
+
+
 ////// 반응형
 var countItemlist = $(".wrap_boxes.board_history .list .content .item").size();
 var countGradulist = $(".box_container .wrap_boxes.graduated .box_item .wrap_list li").size();
-$(".gnb_wrap").show();
+
+// Mobile	
+$(".gnb_wrap").hide();
 $(".overlay").hide();
-$("body").css("overflow-y", "auto");
-$(".wrap_boxes.product .wrap_btns_tab").appendTo(".wrap_boxes.product");
-$(".wrap_boxes.product .box_item").show();
+$(".wrap_boxes.product .wrap_btns_tab").prependTo(".wrap_boxes.product");
+$(".wrap_boxes.product .box_item.prod_advanced").hide();
+$(".wrap_boxes.product .box_item.prod_vip").hide();
 for(i = 0; i < countItemlist ; i++){
-  var thisitemlist = $(".wrap_boxes.board_history .list .content .item").eq(i);
-  thisitemlist.find(".date").before(thisitemlist.find(".point"));
+	var thisitemlist = $(".wrap_boxes.board_history .list .content .item").eq(i);
+	thisitemlist.find(".point").before(thisitemlist.find(".date"));
 };
 for(i = 0; i < countGradulist ; i++){
 	var thisGradulist = $(".box_container .wrap_boxes.graduated .box_item .wrap_list li").eq(i);
-	thisGradulist.find(".wrap_amount").after(thisGradulist.find(".wrap_date"));
+	thisGradulist.find(".wrap_prod").before(thisGradulist.find(".wrap_date"));
 };
-$(".wrap_boxes.subscription .wrap_subs .wrap_btns_tab").appendTo(".wrap_boxes.subscription .wrap_subs");
-$(".wrap_boxes.subscription .wrap_subs .box_item").show();
+$(".wrap_boxes.subscription .wrap_subs .wrap_btns_tab").prependTo(".wrap_boxes.subscription .wrap_subs");
+$(".wrap_boxes.subscription .wrap_subs .box_item.prod_advanced").hide();
+$(".wrap_boxes.subscription .wrap_subs .box_item.prod_vip").hide();
+
+
 
 
 
